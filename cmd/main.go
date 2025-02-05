c03b193f0fd02aa9d5da71986698ffd512973d1a
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-02-05 17:40:03 -0300

    :construction: testing gocron package

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,38 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/SISCONF/sisconf-orders-group-ms.git/internal/jobs"
+	"github.com/go-co-op/gocron/v2"
 )
 
 func main() {
-	// ordersGroup := internal.OrdersGroup{
-	// 	TotalPrice:   64.50,
-	// 	OrderDate:    time.Now(),
-	// 	ItemQuantity: 10,
-	// 	Orders: []internal.Order{
-	// 		{
-	// 			CustomerName: "Alyson",
-	// 			Details: []internal.OrderFood{
-	// 				{
-	// 					FoodName:     "Banana",
-	// 					Quantity:     10,
-	// 					QuantityType: "KG",
-	// 				},
-	// 			},
-	// 		},
-	// 	},
-	// }
-	// err := internal.CreateOrdersGroupXlsxFile(ordersGroup)
-	// if err != nil {
-	// 	fmt.Println("Erro ao criar documento!")
-	// 	return
-	// }
-	// fmt.Println("Documento criado com sucesso!")
-	jobs.SaveAllAvailableFoods()
+	scheduler, err := gocron.NewScheduler()
+	if err != nil {
+		fmt.Println("Error creating scheduler")
+		return
+	}
+
+	_, err = scheduler.NewJob(
+		gocron.DurationJob(
+			10*time.Second,
+		),
+		gocron.NewTask(
+			jobs.SaveAllAvailableFoods,
+		),
+	)
+	if err != nil {
+		fmt.Println("Error creating job!")
+		return
+	}
+	scheduler.Start()
+
+	time.Sleep(time.Minute)
+
+	err = scheduler.Shutdown()
+	if err != nil {
+		fmt.Println("Error during shutdown!")
+	}
 }
