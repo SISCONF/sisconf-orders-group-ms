29a20a001a1ed77f29d96a7b8c4d9898dfb7cf05
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-02-07 22:44:56 -0300

    :sparkles: feat: adds method to writeFoodsList in the xlsX file

--- internal/files/sheets.go
+++ internal/files/sheets.go
@@ -23,6 +23,16 @@ func (foodsList FoodsList) GetSheetColsGroupCount() float64 {
 			float64(sheetFoodRowsPerFoodGroupCount))
 }
 
+func (foodsList FoodsList) GetFoodsNames() []string {
+	var names []string = make([]string, len(foodsList))
+
+	for index, food := range foodsList {
+		names[index] = food.Name
+	}
+
+	return names
+}
+
 func createHeaderRowStyle(file *excelize.File) (int, error) {
 	const headerRowFontColor string = "000000" // Black
 
@@ -49,15 +59,53 @@ func createColsStyle(file *excelize.File) (int, error) {
 	})
 }
 
-func writeOrdersGroupXlsxHeader(file *excelize.File, customerName string) error {
+func writeOrdersGroupXlsxHeader(file *excelize.File, customerName string) (map[int]string, error) {
 	var err error
 	foodsByes, err := os.ReadFile("foods.json")
+	if err != nil {
+		return nil, err
+	}
+
+	var foodsList FoodsList
+	err = json.Unmarshal(foodsByes, &foodsList)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(foodsList) == 0 {
+		return nil, errors.New("foods list is empty")
+	}
+
+	foodsListGroupsCount := foodsList.GetSheetColsGroupCount()
+
+	productsColReference := map[int]string{}
+	var currentCellLetter rune = 'A'
+	for currentCellsGroup := 0.0; currentCellsGroup < foodsListGroupsCount; currentCellsGroup++ {
+		cell := fmt.Sprintf("%c1", currentCellLetter)
+		err = file.SetSheetRow(customerName, cell, &[]any{"KG", "UND", "CX", "PRODUTO"})
+		if err != nil {
+			return nil, err
+		}
+
+		currentCellLetter += sheetColsPerFoodsGroupCount
+		cellToStore := fmt.Sprintf("%c2", currentCellLetter-1)
+		productsColReference[int(currentCellsGroup)] = cellToStore
+	}
+
+	err = file.SetColWidth(customerName, "A", fmt.Sprintf("%c", currentCellLetter), 18)
+
+	return productsColReference, err
+}
+
+func writeFoodsListToGroupXlsx(file *excelize.File, customerName string, productsColReference map[int]string) error {
+	var err error
+	foodsBytes, err := os.ReadFile("foods.json")
 	if err != nil {
 		return err
 	}
 
 	var foodsList FoodsList
-	err = json.Unmarshal(foodsByes, &foodsList)
+	err = json.Unmarshal(foodsBytes, &foodsList)
 	if err != nil {
 		return err
 	}
@@ -66,20 +114,22 @@ func writeOrdersGroupXlsxHeader(file *excelize.File, customerName string) error 
 		return errors.New("foods list is empty")
 	}
 
-	foodsListGroupsCount := foodsList.GetSheetColsGroupCount()
+	foodsGroupQuantity := foodsList.GetSheetColsGroupCount()
 
-	var currentCellLetter rune = 'A'
-	for currentCellsGroup := 0.0; currentCellsGroup < foodsListGroupsCount; currentCellsGroup++ {
-		cell := fmt.Sprintf("%c1", currentCellLetter)
-		err = file.SetSheetRow(customerName, cell, &[]any{"KG", "UND", "CX", "PRODUTO"})
-		if err != nil {
-			return err
+	var startSliceIndex int = 0
+	var endSliceIndex int
+	var foodsNames []string = foodsList.GetFoodsNames()
+
+	for currentFoodGroup := 0.0; currentFoodGroup < foodsGroupQuantity; currentFoodGroup++ {
+		endSliceIndex = (int(currentFoodGroup) + 1) * int(sheetFoodRowsPerFoodGroupCount)
+		if endSliceIndex > len(foodsList) {
+			endSliceIndex = len(foodsList)
 		}
-
-		currentCellLetter += sheetColsPerFoodsGroupCount
+		currentCell := productsColReference[int(currentFoodGroup)]
+		foodsNamesSlice := foodsNames[startSliceIndex:endSliceIndex]
+		err = file.SetSheetCol(customerName, currentCell, &foodsNamesSlice)
+		startSliceIndex += int(sheetFoodRowsPerFoodGroupCount)
 	}
-
-	err = file.SetColWidth(customerName, "A", fmt.Sprintf("%c", currentCellLetter), 18)
 
 	return err
 }
@@ -122,7 +172,12 @@ func CreateOrdersGroupXlsxFile(ordersGroup sisconf.OrdersGroup) error {
 			return err
 		}
 
-		err = writeOrdersGroupXlsxHeader(file, order.CustomerName)
+		productsColReference, err := writeOrdersGroupXlsxHeader(file, order.CustomerName)
+		if err != nil {
+			return err
+		}
+
+		err = writeFoodsListToGroupXlsx(file, order.CustomerName, productsColReference)
 		if err != nil {
 			return err
 		}
