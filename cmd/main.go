61f7a0ce6081328e519de973508c00fbae6e7ab1
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-02-11 21:43:37 -0300

    :sparkles: feat: adds RabbitMQ support and suposedly finishes microservice

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/SISCONF/sisconf-orders-group-ms.git/internal/files"
 	"github.com/SISCONF/sisconf-orders-group-ms.git/internal/jobs"
+	"github.com/SISCONF/sisconf-orders-group-ms.git/internal/sisconf"
+	"github.com/SISCONF/sisconf-orders-group-ms.git/internal/utils"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	utils.PanicOnError("Couldn't load .env file", err)
+
 	scheduler := jobs.NewSisConfCronScheduler()
 	scheduler.Start()
 	log.Println("Successfully started cron scheduler")
+
+	workerQueueName := os.Getenv("RABBIT_MQ_ORDERS_GROUP_SHEET_QUEUE_NAME")
+	msgs := jobs.NewSisconfAmqpBrokerChannel()
+	logMsg := fmt.Sprintf("Waiting for tasks in %s", workerQueueName)
+	log.Println(logMsg)
+
+	go func() {
+		for delivery := range msgs {
+			var ordersGroup sisconf.OrdersGroup
+			err = json.Unmarshal(delivery.Body, &ordersGroup)
+			if err != nil {
+				logMsg = fmt.Sprintf("Couldn't read message body: %s", err.Error())
+				log.Fatalln(logMsg)
+			} else {
+				files.CreateOrdersGroupXlsxFile(ordersGroup)
+			}
+		}
+	}()
 
 	var forever chan any
 
