5cf2045aa832d46560da86239e8abc93c65f7068
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-02-28 20:30:51 -0300

    feature: adds multiple workers

--- cmd/main.go
+++ cmd/main.go
@@ -5,13 +5,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/SISCONF/sisconf-orders-group-ms.git/internal/files"
 	"github.com/SISCONF/sisconf-orders-group-ms.git/internal/jobs"
 	"github.com/SISCONF/sisconf-orders-group-ms.git/internal/sisconf"
 	"github.com/SISCONF/sisconf-orders-group-ms.git/internal/utils"
 	"github.com/joho/godotenv"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
+
+const workerCount int = 5
 
 func main() {
 	err := godotenv.Load()
@@ -28,33 +32,41 @@ func main() {
 	logMsg := fmt.Sprintf("Waiting for tasks in %s", workerQueueName)
 	log.Println(logMsg)
 
-	go func() {
-		for delivery := range msgs {
-			log.Println("Received a task")
+	var wg sync.WaitGroup
 
-			var ordersGroup sisconf.OrdersGroup
-			err = json.Unmarshal(delivery.Body, &ordersGroup)
-			if err != nil {
-				logMsg = fmt.Sprintf("Couldn't read message body: %s", err.Error())
-				log.Println(logMsg)
-				delivery.Nack(false, true)
-				continue
-			}
+	for i := range workerCount {
+		wg.Add(1)
+		go worker(i, msgs, &wg)
+	}
 
-			err = files.CreateOrdersGroupXlsxFile(ordersGroup)
-			if err != nil {
-				logMsg = fmt.Sprintf("Couldn't create spreadsheet: %s", err.Error())
-				log.Println(logMsg)
-				delivery.Nack(false, true)
-				continue
-			}
+	wg.Wait()
+}
 
-			delivery.Ack(false)
-			log.Println("Task acknowledged")
+func worker(id int, msgs <-chan amqp.Delivery, wg *sync.WaitGroup) {
+	defer wg.Done()
+	var logMsg string
+
+	for delivery := range msgs {
+		log.Printf("Worker %d Received a task\n", id)
+
+		var ordersGroup sisconf.OrdersGroup
+		err := json.Unmarshal(delivery.Body, &ordersGroup)
+		if err != nil {
+			logMsg = fmt.Sprintf("Worker %d: Couldn't read message body: %s", id, err.Error())
+			log.Println(logMsg)
+			delivery.Nack(false, true)
+			continue
 		}
-	}()
 
-	var forever chan any
+		err = files.CreateOrdersGroupXlsxFile(ordersGroup)
+		if err != nil {
+			logMsg = fmt.Sprintf("Worker %d: Couldn't create spreadsheet: %s", id, err.Error())
+			log.Println(logMsg)
+			delivery.Nack(false, true)
+			continue
+		}
 
-	<-forever
+		delivery.Ack(false)
+		log.Printf("Worker %d: Task acknowledged\n", id)
+	}
 }
