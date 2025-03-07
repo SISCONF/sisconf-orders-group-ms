515ad327958b09bf3999c7ee1c524aaba6d3a687
gentil-eilison <fernandes.gentilalysonjac@gmail.com>
2025-03-07 11:08:38 -0300

    creates foods json dir

--- internal/jobs/jobs.go
+++ internal/jobs/jobs.go
@@ -2,6 +2,7 @@ package jobs
 
 import (
 	"log"
+	"os"
 
 	"github.com/SISCONF/sisconf-orders-group-ms.git/internal/files"
 	"github.com/SISCONF/sisconf-orders-group-ms.git/internal/sisconf"
@@ -13,6 +14,11 @@ func SaveAllAvailableFoods() {
 	if err != nil {
 		log.Printf("Could not retrieve foods: %s\n", err.Error())
 		return
+	}
+
+	err = os.Mkdir("./internal/data", 0777)
+	if err != nil {
+		log.Printf("Failed to create foods.json dir: %s\n", err.Error())
 	}
 	err = files.WriteApiResponseToJSON(foodsList, "./internal/data/foods.json")
 	if err != nil {
